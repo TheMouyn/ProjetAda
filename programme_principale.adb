aa82b3da38b57dbcde642960ff34e535082f3ab8
oceane <rougier.oceane0708@gmail.com>
2020-11-02 12:27:13 +0100

    Exemple

--- programme_principale.adb
+++ programme_principale.adb
@@ -1,2 +1,10 @@
 with ada.text_io, ada.integer_text_io, ada.float_text_io, Outils, Gestion_Dates, Gestion_Sites, Gestion_Medicament, Gestion_Personnel;
-use ada.text_io, ada.integer_text_io, ada.float_text_io;
+USE Ada.Text_Io, Ada.Integer_Text_Io, Ada.Float_Text_Io;
+
+PROCEDURE Programme_Principale IS
+
+BEGIN
+
+   Put(" ");
+
+End Programme_principale
