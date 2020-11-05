e06c491c26ce4a43cc5163d22c5238ac3ac2944d
oceane <rougier.oceane0708@gmail.com>
2020-11-05 10:45:25 +0100

    Modifications Personnel

--- Gestion_Personnel.adb
+++ Gestion_Personnel.adb
@@ -16,35 +16,6 @@ PACKAGE BODY Gestion_Personnel IS
 
 
 
-   PROCEDURE Ajout (T : IN OUT T_RegistrePersonnel, Laville IN T_Mot; Lenumero IN Integer; RetD,Prod IN Boolean, Ok OUT Boolean) IS
-      --variable ok pour verifier que l'ajout a bien ete realise
-      --procedure ajout d'un nouveau responsable
 
-   BEGIN
-      ok :=false
-         FOR I IN T'RANGE LOOP
-         IF T(I).libre = true THEN
-            --T(I).Numero := Lenumero; doit-on ajouter le numero d'index ?
-            T(I).Ville := Laville;
-            T(I).RetD := True; --pas forcement true ?
-            T(I).Prod := True; --pas forcement true ?
-            Ok := True;
-         END IF;
-      END LOOP;
-   End Ajout;
-
-
---PROCEDURE Fermeture (T: IN OUT T_registreSite) IS --procedure fermeture d'un site
-
-   BEGIN
-      FOR I IN T'RANGE LOOP
-         IF T(I).Libre = False THEN
-            T(I).Ville :=(OTHERS => ' ');
-            T(I).RetD := false;
-            T(I).Prod := false;
-            T(I).Libre:= True;
-         END IF;
-      END LOOP;
-   END Fermeture;
 
 end Gestion_Personnel;
