3768a7f8aece6fa535482a4d20679847e6996eea
Maxime <monillon@outlook.fr>
2020-11-05 16:01:56 +0100

    Ajout NT_console

--- Gestion_Dates.adb
+++ Gestion_Dates.adb
@@ -1,5 +1,5 @@
-with ada.text_io, ada.integer_text_io, ada.float_text_io, Outils, Gestion_Sites, Gestion_Medicament, Gestion_Personnel;
-USE Ada.Text_Io, Ada.Integer_Text_Io, Ada.Float_Text_Io, Outils, Gestion_Sites, Gestion_Medicament, Gestion_Personnel;
+with ada.text_io, ada.integer_text_io, ada.float_text_io, Outils, Gestion_Sites, Gestion_Medicament, Gestion_Personnel, nt_console;
+USE Ada.Text_Io, Ada.Integer_Text_Io, Ada.Float_Text_Io, Outils, Gestion_Sites, Gestion_Medicament, Gestion_Personnel, nt_console;
 
 pragma elaborate_body -- permet de forcer la compilation du body, je pense que l'on pourra le retirer quandon aura des procedure
 
