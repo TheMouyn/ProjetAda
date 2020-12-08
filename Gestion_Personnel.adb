7bd71ec1b262cfc33f6d38bcd23ce837cfcb9b0c
Maxime <monillon@outlook.fr>
2020-12-08 10:18:48 +0100

    suppression ada float

--- Gestion_Personnel.adb
+++ Gestion_Personnel.adb
@@ -1,5 +1,5 @@
-with ada.text_io, ada.integer_text_io, ada.float_text_io, Outils, Gestion_Dates, Gestion_Sites, Gestion_Medicament, nt_console;
-USE Ada.Text_Io, Ada.Integer_Text_Io, Ada.Float_Text_Io, Outils, Gestion_Dates, Gestion_Sites, Gestion_Medicament, nt_console;
+with ada.text_io, ada.integer_text_io, Outils, Gestion_Dates, Gestion_Sites, Gestion_Medicament, nt_console;
+USE Ada.Text_Io, Ada.Integer_Text_Io, Outils, Gestion_Dates, Gestion_Sites, Gestion_Medicament, nt_console;
 
 pragma elaborate_body -- permet de forcer la compilation du body, je pense que l'on pourra le retirer quandon aura des procedure
 
